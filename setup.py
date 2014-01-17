14c58fa4679172193f395774387a241ee0a567bd
Ryan Lane <rlane@lyft.com>
2014-01-16 22:54:50 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "TrebuchetRicochet",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['Flask==0.10.1', 'redis>=2.9.0'],
+
+    author="Ryan Lane",
+    author_email="ryan@ryandlane.com",
+    description="A web interface to trebuchet.",
+    license="apache2",
+    url="https://github.com/trebuchet-deploy/ricochet",
+
+    entry_points = {
+        'console_scripts': [
+            'ricochet = ricochet.runserver:main',
+        ],
+    },
+)
